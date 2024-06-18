444961541b3f4c75d4fb4fe876ccb290d2c60a9f
nick <nick@noreply.com>
2024-06-18 12:45:55 -0400

    little moving icon :D

---
+++ sprite_2d.gd
@@ -0,0 +1,19 @@
+extends Sprite2D
+
+var speed = 400
+var angular_speed = PI
+
+func _process(delta):
+	var direction = 0
+	if Input.is_action_pressed("ui_left"):
+		direction = -1
+	if Input.is_action_pressed("ui_right"):
+		direction = 1
+
+	rotation += angular_speed * direction * delta
+
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2.UP.rotated(rotation) * speed
+
+	position += velocity * delta
