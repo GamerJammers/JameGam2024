d7ad1f1b0e4d43fbe4801d12fc41820517806132
Derek Gooding <djgooding@live.com>
2024-06-18 13:31:26 -0400

    Time to fly!

--- sprite_2d.gd
+++ sprite_2d.gd
@@ -15,5 +15,7 @@ func _process(delta):
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("ui_up"):
 		velocity = Vector2.UP.rotated(rotation) * speed
+	elif Input.is_action_pressed("ui_down"):
+		velocity = -Vector2.UP.rotated(rotation) * speed * speed # So that if you hit the down key, zoom zoom off the screen forever!!!  mwahaha
 
 	position += velocity * delta
