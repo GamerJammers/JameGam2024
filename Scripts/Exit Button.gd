d7ed410d52b89a2d8498ea576871fa5ec49a1fb3
Derek Gooding <djgooding@live.com>
2024-06-20 23:42:04 -0400

    Escape key to exit game
    This is temporary and will instead open a menu later. For now it just exits the game regardless of what scene you're in.

--- Scripts/Exit Button.gd
+++ Scripts/Exit Button.gd
@@ -1,5 +1,9 @@
 extends Button
 
+func _ready():
+	# Connect the button's "pressed" signal to the handler function
+	connect("pressed", Callable(self, "_on_Button_pressed"))
+
 # Function to exit the game
 func _on_Button_pressed():
 	get_tree().quit()
