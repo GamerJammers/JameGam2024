d71072ba291a30c1f3fbecfef8a04ae8bfcc44a7
nick <nick@noreply.com>
2024-06-21 22:42:44 -0400

    updates the loading scene dud

--- Scripts/Start Button.gd
+++ Scripts/Start Button.gd
@@ -5,4 +5,4 @@ func _ready():
 	connect("pressed", Callable(self, "_on_Button_pressed"))
 
 func _on_Button_pressed():
-	get_tree().change_scene_to_file("res://pizza.tscn")
+	get_tree().change_scene_to_file("res://node_2d.tscn")
