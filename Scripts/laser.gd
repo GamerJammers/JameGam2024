2424d134aa961e0eadff2ec62718c2f2494ca337
nick <nick@noreply.com>
2024-06-24 00:15:42 -0400

    more enemy grunt stuff

--- Scripts/laser.gd
+++ Scripts/laser.gd
@@ -1,7 +1,6 @@
-extends RigidBody2D
+class_name Laser extends RigidBody2D
 
 @export var speed := 500.0
-
 var movement_vector := Vector2(0, -1)
 
 func _physics_process(delta):
@@ -9,6 +8,11 @@ func _physics_process(delta):
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
+	
+func power():
+	return 25
+	
+
 
 #func _on_area_entered(area):
 	#if area is Asteroid:
