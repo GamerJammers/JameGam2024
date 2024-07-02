bd5cbb80cb91fffba6d65d96e0b05b26fd6ec810
nick <nick@noreply.com>
2024-07-02 00:32:01 -0400

    refactors some stuff

--- Scripts/ResetButton.gd
+++
@@ -1,9 +0,0 @@
-extends Button
-
-func _ready():
-	# Connect the button's "pressed" signal to the handler function
-	connect("pressed", Callable(self, "_on_Button_pressed"))
-
-func _on_Button_pressed():
-		GameManager.junk = 50
-		GameManager.currency = 0
